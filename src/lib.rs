97bd7dd2b98fc1e07db013a33fc1d1c12e753798
msakuta <masahiro.sakuta@gmail.com>
2020-10-11 18:42:46 +0900

    Add Rust wasm-pack support

---
+++ src/lib.rs
@@ -0,0 +1,209 @@
+mod utils;
+
+use wasm_bindgen::prelude::*;
+use wasm_bindgen::JsCast;
+use web_sys::{WebGlRenderingContext, WebGlProgram, WebGlShader};
+use std::cell::RefCell;
+use std::rc::Rc;
+
+#[wasm_bindgen]
+extern "C" {
+    #[wasm_bindgen(js_namespace = console)]
+    unsafe fn log(s: &str);
+}
+
+macro_rules! console_log {
+    ($fmt:expr, $($arg1:expr),*) => {
+        log(&format!($fmt, $($arg1),+))
+    };
+    ($fmt:expr) => {
+        log($fmt)
+    }
+}
+
+// When the `wee_alloc` feature is enabled, use `wee_alloc` as the global
+// allocator.
+#[cfg(feature = "wee_alloc")]
+#[global_allocator]
+static ALLOC: wee_alloc::WeeAlloc = wee_alloc::WeeAlloc::INIT;
+
+#[wasm_bindgen]
+extern {
+    fn alert(s: &str);
+}
+
+#[wasm_bindgen]
+pub fn greet() {
+    alert("Hello, factorish-js!");
+}
+
+fn window() -> web_sys::Window {
+    web_sys::window().expect("no global `window` exists")
+}
+
+fn request_animation_frame(f: &Closure<dyn FnMut()>) {
+    window()
+        .request_animation_frame(f.as_ref().unchecked_ref())
+        .expect("should register `requestAnimationFrame` OK");
+}
+
+#[wasm_bindgen]
+#[derive(Copy, Clone)]
+struct FactorishState{
+    delta_time: f64,
+}
+
+#[wasm_bindgen]
+impl FactorishState {
+    #[wasm_bindgen(constructor)]
+    pub fn new() -> Result<FactorishState, JsValue> {
+        console_log!("FactorishState constructor");
+
+        Ok(FactorishState{delta_time: 0.1})
+    }
+
+    #[wasm_bindgen]
+    pub fn simulate(&mut self, delta_time: f64) {
+        console_log!("simulating delta_time {}", delta_time);
+        self.delta_time = delta_time;
+    }
+
+    #[wasm_bindgen]
+    pub fn render_init(&self, context: web_sys::WebGlRenderingContext) -> Result<(), JsValue> {
+            
+        let vert_shader = compile_shader(
+            &context,
+            WebGlRenderingContext::VERTEX_SHADER,
+            r#"
+            attribute vec4 position;
+            void main() {
+                gl_Position = position;
+            }
+        "#,
+        )?;
+        let frag_shader = compile_shader(
+            &context,
+            WebGlRenderingContext::FRAGMENT_SHADER,
+            r#"
+            void main() {
+                gl_FragColor = vec4(1.0, 1.0, 1.0, 1.0);
+            }
+        "#,
+        )?;
+        let program = link_program(&context, &vert_shader, &frag_shader)?;
+        context.use_program(Some(&program));
+        Ok(())
+    }
+
+    #[wasm_bindgen]
+    pub fn render(&self, context: web_sys::WebGlRenderingContext) -> Result<(), JsValue> {
+        // ctx.set_line_width(10.);
+        // ctx.stroke_rect(75., 140., 150., 110.);
+        
+        let vertices: [f32; 9] = [-0.7, -0.7, 0.0, 0.7, -0.7, 0.0, 0.0, 0.7, 0.0];
+
+        let buffer = context.create_buffer().ok_or("failed to create buffer")?;
+        context.bind_buffer(WebGlRenderingContext::ARRAY_BUFFER, Some(&buffer));
+
+        // Note that `Float32Array::view` is somewhat dangerous (hence the
+        // `unsafe`!). This is creating a raw view into our module's
+        // `WebAssembly.Memory` buffer, but if we allocate more pages for ourself
+        // (aka do a memory allocation in Rust) it'll cause the buffer to change,
+        // causing the `Float32Array` to be invalid.
+        //
+        // As a result, after `Float32Array::view` we have to be very careful not to
+        // do any memory allocations before it's dropped.
+        unsafe {
+            let vert_array = js_sys::Float32Array::view(&vertices);
+
+            context.buffer_data_with_array_buffer_view(
+                WebGlRenderingContext::ARRAY_BUFFER,
+                &vert_array,
+                WebGlRenderingContext::STATIC_DRAW,
+            );
+        }
+
+        context.vertex_attrib_pointer_with_i32(0, 3, WebGlRenderingContext::FLOAT, false, 0, 0);
+        context.enable_vertex_attrib_array(0);
+
+        context.clear_color(0.0, 0.0, 0.0, 1.0);
+        context.clear(WebGlRenderingContext::COLOR_BUFFER_BIT);
+
+        context.draw_arrays(
+            WebGlRenderingContext::TRIANGLES,
+            0,
+            (vertices.len() / 3) as i32,
+        );
+
+        Ok(())
+    }
+}
+
+pub fn compile_shader(
+    context: &WebGlRenderingContext,
+    shader_type: u32,
+    source: &str,
+) -> Result<WebGlShader, String> {
+    let shader = context
+        .create_shader(shader_type)
+        .ok_or_else(|| String::from("Unable to create shader object"))?;
+    context.shader_source(&shader, source);
+    context.compile_shader(&shader);
+
+    if context
+        .get_shader_parameter(&shader, WebGlRenderingContext::COMPILE_STATUS)
+        .as_bool()
+        .unwrap_or(false)
+    {
+        Ok(shader)
+    } else {
+        Err(context
+            .get_shader_info_log(&shader)
+            .unwrap_or_else(|| String::from("Unknown error creating shader")))
+    }
+}
+
+pub fn link_program(
+    context: &WebGlRenderingContext,
+    vert_shader: &WebGlShader,
+    frag_shader: &WebGlShader,
+) -> Result<WebGlProgram, String> {
+    let program = context
+        .create_program()
+        .ok_or_else(|| String::from("Unable to create shader object"))?;
+
+    context.attach_shader(&program, vert_shader);
+    context.attach_shader(&program, frag_shader);
+    context.link_program(&program);
+
+    if context
+        .get_program_parameter(&program, WebGlRenderingContext::LINK_STATUS)
+        .as_bool()
+        .unwrap_or(false)
+    {
+        Ok(program)
+    } else {
+        Err(context
+            .get_program_info_log(&program)
+            .unwrap_or_else(|| String::from("Unknown error creating program object")))
+    }
+}
+
+#[wasm_bindgen]
+pub fn simulate() -> Result<(), JsValue> {
+    console_log!("Running simulate");
+
+
+    let func = Rc::new(RefCell::new(None));
+    let g = func.clone();
+
+    *g.borrow_mut() = Some(Closure::wrap(Box::new(move || {
+        console_log!("animating simulate");
+        // Schedule ourself for another requestAnimationFrame callback.
+        request_animation_frame(func.borrow().as_ref().unwrap());
+    }) as Box<dyn FnMut()>));
+
+    request_animation_frame(g.borrow().as_ref().unwrap());
+
+    Ok(())
+}
