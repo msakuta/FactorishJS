1f52c518cdabc418cc8db242bb7f350c4c2971b4
msakuta <masahiro.sakuta@gmail.com>
2020-10-13 02:02:34 +0900

    Remove unnecessary bitshifts from JavaScript implementation
    They were used to ensure the variable is integral type, but
    Rust is explicit about types.

--- src/perlin_noise.rs
+++ src/perlin_noise.rs
@@ -61,7 +61,7 @@ fn crc32_gp(str: &[u32]) -> u32 {
         }
     }
 
-    (crc ^ 0xffffffff) >> 0
+    crc ^ 0xffffffff
 }
 struct Xor128 {
     x: u32,
@@ -71,7 +71,7 @@ impl Xor128 {
     fn new(seed: u32) -> Self {
         let mut ret = Xor128 { x: 2463534242 };
         if 0 < seed {
-            ret.x ^= (seed & 0xffffffff) >> 0;
+            ret.x ^= seed;
             ret.nexti();
         }
         ret.nexti();
@@ -79,13 +79,11 @@ impl Xor128 {
     }
 
     fn nexti(&mut self) -> u32 {
-        // We must bitmask and logical shift to simulate 32bit unsigned integer's behavior.
-        // The optimizer is likely to actually make it uint32 internally (hopefully).
         // T = (I + L^a)(I + R^b)(I + L^c)
         // a = 13, b = 17, c = 5
-        let x1 = ((self.x ^ (self.x << 13)) & 0xffffffff) >> 0;
-        let x2 = ((x1 ^ (x1 >> 17)) & 0xffffffff) >> 0;
-        self.x = ((x2 ^ (x2 << 5)) & 0xffffffff) >> 0;
+        let x1 = self.x ^ (self.x << 13);
+        let x2 = x1 ^ (x1 >> 17);
+        self.x = x2 ^ (x2 << 5);
         self.x
     }
 
